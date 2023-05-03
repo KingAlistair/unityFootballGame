eacdf8a06ce9f721642842a3d6734749e1b3d229
Dániel Szabó <kingalistairlol@gmail.com>
2023-05-03 16:48:03 +0200

    Pushing initial project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
+        characterController.Move(movement);
+    }
+}
