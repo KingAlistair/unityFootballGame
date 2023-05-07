0f92873e9a50c4845dbb4adfd27e62abf33b3fd2
Szabó Dániel <kingalistairlol@gmail.com>
2023-05-07 21:27:41 +0200

    Added kick

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,10 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 15f;
     private CharacterController characterController;
+    public GameObject leftLeg;
+    public float kickAngle = 90f;
+    public float kickForce = 10f;
+    public float kickDistance = 1f;
 
     void Start()
     {
@@ -17,5 +21,39 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
         characterController.Move(movement);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Kick();
+        }
+    }
+
+    void Kick()
+    {
+        // Rotate the LeftLeg gameobject to mimic kicking the ball
+        leftLeg.transform.localRotation = Quaternion.Euler(-kickAngle, 0f, 0f);
+
+        // Check if there is a ball nearby
+        Collider[] colliders = Physics.OverlapSphere(leftLeg.transform.position, kickDistance);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Ball"))
+            {
+                Rigidbody rb = collider.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    // Apply a force to the ball to simulate kicking it
+                    rb.AddForce(leftLeg.transform.forward * kickForce, ForceMode.Impulse);
+                }
+            }
+        }
+
+        // Rotate the LeftLeg gameobject back to its original position after a short delay
+        Invoke("ResetLegRotation", 0.5f);
+    }
+
+    void ResetLegRotation()
+    {
+        leftLeg.transform.localRotation = Quaternion.identity;
     }
 }
